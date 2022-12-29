427e56548f81f973280fbea3c6b30d3fc733b885
pravinbhagwat <pravinbhagwat150@gmail.com>
2022-12-29 12:13:58 +0530

    Second Build

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -9,6 +9,7 @@ public class MovieRepository {
 
     HashMap<String, Movie> movieDb = new HashMap<>();
     HashMap<String, Director> directorDb = new HashMap<>();
+    HashMap<Director, List<Movie>> movieDirectorDb = new HashMap<>();
     public String addMovieInDb(Movie movie) {
         String movieName = movie.getName();
         movieDb.put(movieName, movie);
@@ -38,15 +39,58 @@ public class MovieRepository {
     }
 
     public String deleteDirectorByNameFromDb(String directorName) {
+        Director director = directorDb.get(directorName);
+        List<Movie> movieList = movieDirectorDb.get(director);
+        for (Movie movie : movieList) {
+            movieDb.remove(movie.getName());
+        }
+        directorDb.remove(directorName);
+        movieDirectorDb.remove(director);
 
+        return "Successfully deleted movies related to given director";
     }
 
     public String deleteAllDirectorsFromDb() {
+        List<Director> directors= new ArrayList<>();
+        for (Map.Entry<Director, List<Movie>> set :
+                movieDirectorDb.entrySet()) {
+            List<Movie> movieList = set.getValue();
+            for (Movie movie : movieList) {
+                movieDb.remove(movie.getName());
+            }
+
+            directors.add(set.getKey());
+            directorDb.remove(set.getKey().getName());
+        }
+        for (Director director : directors
+        ) {
+            directorDb.remove(director.getName());
+        }
+        return "Successfully deleted all directors and movies";
     }
 
     public String addMovieDirectorPair(String movieName, String directorName) {
+        Movie movie = getMovieByNameFromDb(movieName);
+        if (movieDirectorDb.containsKey(getDirectorByNameFromDb(directorName))) {
+
+            List<Movie> movieList = movieDirectorDb.get(getDirectorByNameFromDb(directorName));
+            movieList.add(movie);
+            movieDirectorDb.put(getDirectorByNameFromDb(directorName), movieList);
+        } else {
+            List<Movie> movieList = new ArrayList<>();
+            movieList.add(movie);
+            movieDirectorDb.put(getDirectorByNameFromDb(directorName), movieList);
+        }
+        return "Successfully created movie director pair";
     }
 
     public List<String> getMoviesByDirectorName(String directorName) {
+        Director director = directorDb.get(directorName);
+        List<Movie> movieList = movieDirectorDb.get(director);
+        List<String> movieNameList = new ArrayList<>();
+        for (Movie movie : movieList) {
+            movieNameList.add(movie.getName());
+        }
+        return movieNameList;
     }
 }
